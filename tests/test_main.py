5bf63c4ca55da0b6b3a9f3dadbce4d71b458c316
Alex Zhidkov <sashazudkov.@gmail.com>
2022-02-04 11:51:47 +0200

    Solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -26,24 +26,6 @@ thirty_days = thirty_days_or_less()
                 thirty_days.day,
                 "Password is actual."
         ),
-        (
-                thirty_days.year,
-                thirty_days.month,
-                thirty_days.day,
-                "Password is actual."
-        ),
-        (
-                thirty_days.year,
-                thirty_days.month,
-                thirty_days.day,
-                "Password is actual."
-        ),
-        (
-                thirty_days.year,
-                thirty_days.month,
-                thirty_days.day,
-                "Password is actual."
-        ),
     ],
 )
 def test_30_days_or_less(year, month, day, expected):
@@ -70,24 +52,6 @@ more_thirty_less_sixty = more_thirty_days_less_sixty()
                 more_thirty_less_sixty.day,
                 "You should change your password."
         ),
-        (
-                more_thirty_less_sixty.year,
-                more_thirty_less_sixty.month,
-                more_thirty_less_sixty.day,
-                "You should change your password."
-        ),
-        (
-                more_thirty_less_sixty.year,
-                more_thirty_less_sixty.month,
-                more_thirty_less_sixty.day,
-                "You should change your password."
-        ),
-        (
-                more_thirty_less_sixty.year,
-                more_thirty_less_sixty.month,
-                more_thirty_less_sixty.day,
-                "You should change your password."
-        ),
     ],
 )
 def test_more_30_days_less_60(year, month, day, expected):
@@ -108,24 +72,6 @@ sixty_days = more_sixty_days()
 @pytest.mark.parametrize(
     "year, month, day, expected",
     [
-        (
-                sixty_days.year,
-                sixty_days.month,
-                sixty_days.day,
-                "Immediately change the password!"
-        ),
-        (
-                sixty_days.year,
-                sixty_days.month,
-                sixty_days.day,
-                "Immediately change the password!"
-        ),
-        (
-                sixty_days.year,
-                sixty_days.month,
-                sixty_days.day,
-                "Immediately change the password!"
-        ),
         (
                 sixty_days.year,
                 sixty_days.month,
