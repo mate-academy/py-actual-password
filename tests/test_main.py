12ef86a75e0059f36cc2edee2e473547fd29de02
Alex Zhidkov <sashazudkov.@gmail.com>
2022-02-14 16:54:13 +0200

    del random tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,84 +1,64 @@
 import datetime
-import random
-
 import pytest
 
 from app.main import actual_password
 
 
-def thirty_days_or_less():
-    today = datetime.date.today()
-    random_day = random.randint(1, 30)
-
-    result_date = today - datetime.timedelta(days=random_day)
-    return result_date
-
-
-thirty_days = thirty_days_or_less()
-
-
 @pytest.mark.parametrize(
-    "year, month, day, expected",
+    "days, expected",
     [
-        (
-                thirty_days.year,
-                thirty_days.month,
-                thirty_days.day,
-                "Password is actual."
-        ),
+        (0, "Password is actual."),
+        (1, "Password is actual."),
+        (15, "Password is actual."),
+        (29, "Password is actual."),
+        (30, "Password is actual."),
+
     ],
 )
-def test_30_days_or_less(year, month, day, expected):
+def test_30_days_or_less(days, expected):
+    today = datetime.date.today()
+    actual_date = today - datetime.timedelta(days)
+    year = actual_date.year
+    month = actual_date.month
+    day = actual_date.day
     assert actual_password(year, month, day) == expected
 
 
-def more_thirty_days_less_sixty():
-    today = datetime.date.today()
-    random_day = random.randint(31, 60)
-
-    result_date = today - datetime.timedelta(days=random_day)
-    return result_date
-
-
-more_thirty_less_sixty = more_thirty_days_less_sixty()
-
-
 @pytest.mark.parametrize(
-    "year, month, day, expected",
+    "days, expected",
     [
-        (
-                more_thirty_less_sixty.year,
-                more_thirty_less_sixty.month,
-                more_thirty_less_sixty.day,
-                "You should change your password."
-        ),
+        (31, "You should change your password."),
+        (45, "You should change your password."),
+        (50, "You should change your password."),
+        (59, "You should change your password."),
+        (60, "You should change your password."),
+
     ],
 )
-def test_more_30_days_less_60(year, month, day, expected):
+def test_more_30_days_less_60(days, expected):
+    today = datetime.date.today()
+    actual_date = today - datetime.timedelta(days)
+    year = actual_date.year
+    month = actual_date.month
+    day = actual_date.day
     assert actual_password(year, month, day) == expected
 
 
-def more_sixty_days():
-    today = datetime.date.today()
-    random_day = random.randint(61, 10000)
-
-    result_date = today - datetime.timedelta(days=random_day)
-    return result_date
-
-
-sixty_days = more_sixty_days()
-
-
 @pytest.mark.parametrize(
-    "year, month, day, expected",
+    "days, expected",
     [
-        (
-                sixty_days.year,
-                sixty_days.month,
-                sixty_days.day,
-                "Immediately change the password!"
-        ),
+        (61, "Immediately change the password!"),
+        (99, "Immediately change the password!"),
+        (100, "Immediately change the password!"),
+        (1000, "Immediately change the password!"),
+        (10000, "Immediately change the password!"),
+
     ],
 )
-def test_more_60_days(year, month, day, expected):
+def test_more_60_days(days, expected):
+    today = datetime.date.today()
+    actual_date = today - datetime.timedelta(days)
+    year = actual_date.year
+    month = actual_date.month
+    day = actual_date.day
     assert actual_password(year, month, day) == expected
